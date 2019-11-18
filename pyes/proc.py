cfd25545f1e76c98ba0014e4e185cd0e3c1839f8
mdjogatovic <djogatovicm@gmail.com>
2019-11-19 00:28:19 +0100

    Update pyes

--- pyes/proc.py
+++ pyes/proc.py
@@ -1,4 +1,18 @@
 """
+# PYES - PYthon Event Scheduling simulation library
+
+Easy to use Python simulation library based on event scheduling strategy.
+
+Author: Marko S. Djogatovic
+
+Licence: MIT
+
+## Installation
+python setup.py install
+
+## Requirements
+1. Python >= 3.6
+
 Process objects - resource, queue
 """
 import pyes.base
--- pyes/stats.py
+++ pyes/stats.py
@@ -1,3 +1,20 @@
+"""
+# PYES - PYthon Event Scheduling simulation library
+
+Easy to use Python simulation library based on event scheduling strategy.
+
+Author: Marko S. Djogatovic
+
+Licence: MIT
+
+## Installation
+python setup.py install
+
+## Requirements
+1. Python >= 3.6
+
+Statistics
+"""
 from pyes.base import clock, elapsed_time, CONF
 
 class stats:
